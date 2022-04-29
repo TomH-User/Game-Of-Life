84f950ffb2807019a48907c23ca527afb90b4d9f
Tom <73723037+TomH-User@users.noreply.github.com>
2022-04-29 15:47:59 +0200

    Update main.c

--- SDL/src/main.c
+++ SDL/src/main.c
@@ -84,10 +84,14 @@ int main (int argc, char **argv) {
 }
 
 
-/* 
-   Ligne de commande à écrire pour la compilation du programme :
+
+/* ----- -POUR POUVOIR COMPILER ET EXECUTER LE PROGRAMME ------------
+
+   Pour la compilation 
+-> Se placer dans le dossier SDL et écrire en ligne de commmande :
     gcc src/*.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2  
 
-   Pour éxécuter le programme : 
+   Pour l'éxécution 
+-> Se placer dans le dossier SDL et écrire en ligne de commmande :
     bin\prog.exe                                                            
 */
